571cd02d6e14c40ab6232f0f1fbd60d0846f6047
kiyongee2 <kiyongee2@gmail.com>
2023-05-24 15:05:00 +0900

    enum

--- ch04/src/enums/UsingDefine.java
+++ ch04/src/enums/UsingDefine.java
@@ -6,4 +6,5 @@ public class UsingDefine {
 	public static final int MAX_NUM = 100;
 	public static final int MIN_NUM = 1;
 	public static final double PI = 3.1415;
+	public static final double EARTH_RADIUS = 6400;  //지구의 반지름(KM)
 }
