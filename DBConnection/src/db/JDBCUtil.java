cff5b886e506918e2ba6e8b2da78ebdbb7309d8f
kiyongee2 <kiyongee2@gmail.com>
2023-05-25 18:20:35 +0900

    db connection

---
+++ DBConnection/src/db/JDBCUtil.java
@@ -0,0 +1,27 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//DB 연결 및 종료 기능을 하는 클래스
+public class JDBCUtil {
+	//필드
+	static String driverClass = "oracle.jdbc.OracleDriver"; //오라클 드라이버
+	static String url = "jdbc:oracle:thin:@localhost:1521:xe"; //db 경로(위치)
+	static String username = "c##mydb";
+	static String password = "mydb";
+	
+	//DB 연결 메서드
+	public static Connection getConnection() {
+		try {
+			Class.forName(driverClass);
+			return DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
